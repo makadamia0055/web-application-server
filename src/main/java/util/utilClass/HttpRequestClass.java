8a8ce60143910a2f808aeeb3c56f211df5d96532
makadamia0055 <makend91@naver.com>
2024-06-19 18:04:41 +0900

    HttpRequestClass 수정

--- src/main/java/util/utilClass/HttpRequestClass.java
+++ src/main/java/util/utilClass/HttpRequestClass.java
@@ -5,65 +5,46 @@ import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
 import webserver.RequestHandler;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.StringTokenizer;
 
 public class HttpRequestClass {
-
-    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
-
     private HttpMethod method;
-
-    private String url;
     private String path;
-    private Map<String, String> param;
+    private String version;
+    private Map<String, String> headers;
+    private String body;
 
-    private String httpVersion;
-
-    public HttpRequestClass(String httpHeader){
-        StringTokenizer st = new StringTokenizer(httpHeader);
-        this.method = HttpMethod.valueOf(st.nextToken());
-        this.url = st.nextToken();
-        this.httpVersion = st.nextToken();
-
-        if(url.contains("/")){
-            String rawPath = url.substring(url.indexOf("/"));
-            if(rawPath.contains("?")){
-                int index = rawPath.indexOf("?");
-                this.path = rawPath.substring(0, index);
-                String paramStr = rawPath.substring(index+1);
-                this.param = HttpRequestUtils.parseQueryString(paramStr);
-
-            }else {
-                this.path = rawPath;
-            }
-
-        }
-
+    public HttpRequestClass(String method, String path, String version, Map<String, String> headers) {
+        this.method = HttpMethod.valueOf(method);
+        this.path = path;
+        this.version = version;
+        this.headers = headers;
     }
 
-    public HttpMethod getMethod() {
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getMethod() {
         return method;
     }
 
-    public String getUrl() {
-        return url;
-    }
-
-    public String getPath(){
+    public String getPath() {
         return path;
     }
-    public Optional<Map<String, String>> getParam(){
-        return Optional.ofNullable(param);
+
+    public String getVersion() {
+        return version;
     }
 
-    @Override
-    public String toString() {
-        return "HttpRequestClass{" +
-                "method=" + method +
-                ", url='" + url + '\'' +
-                ", httpVersion='" + httpVersion + '\'' +
-                '}';
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public String getBody() {
+        return body;
     }
 }
