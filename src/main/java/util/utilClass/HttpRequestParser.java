0d22a4c60d2355235521b5dcf36b3dc3482246f2
makadamia0055 <makend91@naver.com>
2024-06-19 18:04:30 +0900

    HttpRequestParser 추가

---
+++ src/main/java/util/utilClass/HttpRequestParser.java
@@ -0,0 +1,70 @@
+package util.utilClass;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class HttpRequestParser {
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+
+    public static Optional<HttpRequestClass> extractHttpRequest(BufferedReader br) throws IOException{
+        try{
+            String requestLine = br.readLine();
+
+            if(requestLine==null||requestLine.isEmpty()){
+                return Optional.empty();
+            }
+
+            // 요청 라인 공백으로 분리하여 메서드, 경로, 버전 추출
+            String[] requestLineParts = requestLine.split(" ");
+            if(requestLineParts.length<3){
+                return Optional.empty();
+            }
+            String method = requestLineParts[0];
+            String url = requestLineParts[1];
+            String version = requestLineParts[2];
+
+            // 헤더를 읽어 Map로 정리
+            Map<String, String> headers = new HashMap<>();
+            String line;
+            while(!(line=br.readLine()).isEmpty()){
+                String[] headerParts = line.split(":", 2);
+                if(headerParts.length==2){
+                    headers.put(headerParts[0].trim(), headerParts[1].trim());
+                }
+            }
+            HttpRequestClass httpRequestClass = new HttpRequestClass(method, url, version, headers);
+
+            // Content-Length 헤더가 있으면, 해당 길이만큼 바디를 읽음
+            String contentLengthHeader = headers.get("Content-Length");
+            if(contentLengthHeader != null){
+                int contentLength = Integer.parseInt(contentLengthHeader);
+                char[] bodyChars = new char[contentLength];
+                int read =  br.read(bodyChars, 0, contentLength);
+                String body = new String(bodyChars, 0, read);
+                log.info(body);
+                httpRequestClass.setBody(body);
+            }else{
+                // Content-length가 없는 경우 바디 전체를 읽기
+                String body = br.lines().collect(Collectors.joining("\n"));
+                log.info(body);
+                httpRequestClass.setBody(body);
+            }
+            return Optional.of(httpRequestClass);
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+            return Optional.empty();
+        }
+
+
+    }
+}
